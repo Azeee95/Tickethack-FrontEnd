bf22a444d756bc2d565b187df5ff680890b522dc
Kodzo <Kodzo@kovalys-cloud.com>
2023-09-20 09:24:46 +0200

    Commit new try time display

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ searchButton.addEventListener('click', () => {
 
                // let dateTrip = new Date(data[i].date);
 
-                console.log((data[i].date).substr(11, 5));
+              //  console.log((data[i].date).substr(11, 5));
 
                 document.querySelector("#resultcart").innerHTML += ` 
             
