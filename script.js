a9fb3c517b193091f52300cd8f0e1e4ffc1b62b2
Kodzo <Kodzo@kovalys-cloud.com>
2023-09-20 09:22:59 +0200

    Commit new try time display

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ searchButton.addEventListener('click', () => {
                             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                         </svg>
                 
-                        <a class="link-body-emphasis text-decoration-none"> 13:43 </a>
+                        <a class="link-body-emphasis text-decoration-none"> ${(data[i].date).substr(11, 5)} </a>
                         
                       </li>
                 
