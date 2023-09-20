dda973e8f714155ece84426aef756cb306c7ac6f
Kodzo <Kodzo@kovalys-cloud.com>
2023-09-20 15:02:23 +0200

    Commit apres conditions cart

--- scriptcart.js
+++ scriptcart.js
@@ -67,7 +67,7 @@
                               <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                           </svg>
                   
-                          <a class="link-body-emphasis text-decoration-none">${cartData[i].price} € </a>
+                          <a class="link-body-emphasis text-decoration-none price">${cartData[i].price} € </a>
                           
                         </li>
                   
@@ -128,6 +128,14 @@
                 .then(data => {
 
                 console.log(data);
+
+                })
+                .then (() => {
+
+                
+                document.querySelector('#totalcart').textContent = `Total : ${totalCart} €`
+
+
 
                 })
 
